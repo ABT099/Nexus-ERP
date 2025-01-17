2ec9c89cfedd68427f1452eb2ec1ab71d53b714f
ABT099 <abdo.formal99@gmail.com>
2025-01-17 15:01:15 +0300

    add swagger docs

---
+++ src/main/java/com/nexus/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.nexus.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "NEXUS Api",
+                description = "This api is developed for the Nexus system and is not for public use, this is just a demo",
+                version = "1.0"
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
--- src/main/java/com/nexus/config/SecurityConfig.java
+++ src/main/java/com/nexus/config/SecurityConfig.java
@@ -35,7 +35,19 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("auth/login").permitAll()
+                        .requestMatchers(
+                                "auth/login",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui/**",
+                                "/webjars/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .requestMatchers(HttpMethod.POST, "admins").permitAll()
                         .anyRequest().authenticated()
                 ).httpBasic(Customizer.withDefaults())
